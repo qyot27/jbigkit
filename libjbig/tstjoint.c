3304b797f88060ea610bca324c0fd8664b0a8072
Markus Kuhn <Markus.Kuhn@cl.cam.ac.uk>
2014-05-01 14:48:33 +0100

    added test for linking with both jbig.c and jbig85.c simultaneously

---
+++ libjbig/tstjoint.c
@@ -0,0 +1,14 @@
+/*
+ *  This test file exists to check for any compile-time problems
+ *  when linking an application with both jbig.c and jbig85.c simultanenously.
+ *
+ *  Markus Kuhn -- http://www.cl.cam.ac.uk/~mgk25/
+ */
+
+#include "jbig.h"
+#include "jbig85.h"
+
+int main()
+{
+  return 0;
+}
